e328f42a9d281638a46a3eed8083047289a97890
Molham Al khodari <alkhodari@drherwig.de>
2022-05-27 15:10:04 +0200

    first commit

---
+++ src/com/exercise/Exercise.java
@@ -0,0 +1,71 @@
+package com.exercise;
+
+public class Exercise {
+    public static void main(String[] args) {
+
+        // Übung_01
+        // Fügen Sie den fehlenden Teil "code" des Codes unten ein, um "Hello World" auszugeben.
+
+        code.code.code("Hello World");
+
+        //-------------------------------------------------------------------------------------//
+
+        // Übung_02
+        // Kommentare werden in Java mit Sonderzeichen geschrieben. Fügen Sie die fehlenden Teile ein:
+
+        This is a single-line comment
+        This is a multi-line comment
+
+        //-------------------------------------------------------------------------------------//
+
+
+        // Übung_03
+        // Erstellen Sie eine Variable namens carName und weisen Sie ihr den Wert Volvo zu.
+
+        code code = "code"
+
+        //-------------------------------------------------------------------------------------//
+
+        // Übung_04
+        // Fügen Sie den richtigen Datentyp für die folgenden Variablen hinzu:
+
+        code myNum = 9;
+        code myFloatNum = 8.99f;
+        code myLetter = 'A';
+        code myBool = false;
+        code myText = "Hello World";
+
+        //-------------------------------------------------------------------------------------//
+
+        // Übung_05
+        // Gibt „Hello World“ aus, wenn x größer als y ist.
+
+        int x = 50;
+        int y = 10;
+
+        if (x code y) {
+            System.out.println("Hello World");
+        }
+
+        //-------------------------------------------------------------------------------------//
+
+        // Übung_06
+        // Verwenden Sie eine for-Schleife, um fünfmal "Ja" auszugeben:
+
+        code (int i = 0; i < 5; code) {
+            System.out.println(code);
+        }
+
+        //-------------------------------------------------------------------------------------//
+
+        // Übung_07
+        // Verwenden Sie eine for-Schleife, um fünfmal "Ja" auszugeben:
+
+        code (int i = 0; i < 5; code) {
+            System.out.println(code);
+        }
+
+        //-------------------------------------------------------------------------------------//
+
+    }
+}
